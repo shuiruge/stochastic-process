36a5554dbfd354ef95ce06108d07b6410c5a1d6f
shuiruge <shuiruge@whu.edu.cn>
2025-07-05 19:38:47 +0800

    Using scale factor with sparsity or not.

--- scripts/perceptron_sparsity_fisher.py
+++ scripts/perceptron_sparsity_fisher.py
@@ -10,15 +10,18 @@ from tqdm import tqdm
 from fisher import Fisher
 
 # Dynamics of perceptron with sparse weights.
-def perceptron_dynamics(dim, sparsity, activation):
+def perceptron_dynamics(dim, sparsity, activation, scale_by_sparsity):
     assert sparsity > 0
     mask = random.bernoulli(random.key(42), sparsity, [dim, dim])
     mask = mask.astype('float64')
     W = mask * random.normal(random.key(42), [dim, dim])
     def dynamics(W, x):
-        scale = np.sqrt(12 / sparsity / dim)
+        if scale_by_sparsity:
+            scale = np.sqrt(12 / sparsity / dim)
+        else:  # use the standard scale factor of perceptron.
+            scale = np.sqrt(12 / dim)
         return activation(scale * x @ W)
-    return dynamics
+    return dynamics, W
 
 def variance(x, axis=None):
     mean = np.mean(x, axis=axis, keepdims=True)
@@ -33,16 +36,18 @@ dt, T = 1e-2, 1e+0
 nonzero_fisher_vals = []
 sparsities = 10**np.linspace(-5, 0, 30)
 for sparsity in tqdm(sparsities):
-    dynamics = perceptron_dynamics(dim, sparsity, np.tanh)
+    dynamics, W = perceptron_dynamics(dim, sparsity, np.tanh, False)
     #print(np.mean(std(dynamics(W, random.normal(random.key(42), [100, dim])), axis=1)))
     fisher = Fisher(dynamics, temperature=1e-0)
     fisher_val, final_x = fisher(W, x, dt, T)
     nonzero_fisher_val = sparse.BCOO.fromdense(fisher_val).data
     nonzero_fisher_vals.append(nonzero_fisher_val)
 
-plt.loglog(sparsities, [variance(_) for _ in nonzero_fisher_vals],
+nonzero_fisher_vars = [variance(_) for _ in nonzero_fisher_vals]
+plt.loglog(sparsities, nonzero_fisher_vars,
            'o-', alpha=0.5)
+plt.vlines(1/dim, 0, max(nonzero_fisher_vars),
+           linestyles="dotted", colors="k")
 plt.xlabel(r'sparsity $\theta$')
-plt.ylabel(rf'$\textrm{Var} \left( F(\theta, {T}) \right)$ '
-           'on nonzero indices')
+plt.ylabel(rf'Variation of $F(\theta, {T})$ on nonzero indices')
 plt.show()
